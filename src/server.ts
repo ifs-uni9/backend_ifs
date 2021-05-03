2eddcca65e17a75f2612abea575c81af379bb05d
Bryan <bryan.matheusbmf@gmail.com>
2021-05-02 22:07:00 -0300

    :sparkles: add morgan as logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import { config } from "dotenv";
 import express from "express";
+import morgan from "morgan";
 
 config();
+
 const app = express();
+
+app.use(morgan("combined"));
+app.get("/", function (req, res) {
+  res.send("ok");
+});
 
 app.listen(process.env.SR_PORT || 3333);
