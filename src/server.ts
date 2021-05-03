7efbdb39dab14bde76b9155ef425b656b092d376
Bryan <bryan.matheusbmf@gmail.com>
2021-05-02 22:11:30 -0300

    :sparkles: feat: add first route

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ config();
 const app = express();
 
 app.use(morgan("combined"));
-app.get("/", function (req, res) {
-  res.send("ok");
+app.get("/api", (_, res) => {
+  res.send("API IFS for backend v1.0.0.");
 });
 
 app.listen(process.env.SR_PORT || 3333);
